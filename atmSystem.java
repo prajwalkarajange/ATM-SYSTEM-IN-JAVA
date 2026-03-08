ff5fc023c83c57a9680a8b1154e38b1a8bcd46f5
prajwalkarajange <prajwalkarajange0409@gmail.com>
2026-03-08 09:59:04 +0530

    Fix formatting in ATM system menu output
    The Output formate is Fixed

--- atmSystem.java
+++ atmSystem.java
@@ -13,7 +13,7 @@ public class atmSystem {
         }
         while (flag) {
             System.out.println();
-            System.out.println("*************************************************");
+            System.out.println("***************************************************");
             System.out.println("Enter choice: ");
             System.out.println("What do you want to do?");
             System.out.println("1: Check Balance\n2: Deposit\n3: Withdraw\n4: Exit");
